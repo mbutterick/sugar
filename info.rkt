b32c555a890ea53fff5c6a76f71aa5dfed77e44a
Matthew Butterick <mb@mbtype.com>
2014-10-19 10:29:06 -0700

    udpate dependencies

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang info
 (define collection "sugar")
 (define deps '(("base" #:version "6.0")))
+(define build-deps '("scribble-lib"))
 (define scribblings '(("scribblings/sugar.scrbl" ())))
+(define compile-omit-paths '("tests.rkt"))
