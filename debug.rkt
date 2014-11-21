3426730859e7f32735d6efc6258da9eac9ba5346
Matthew Butterick <mb@mbtype.com>
2014-11-20 16:15:31 -0800

    add report*

--- debug.rkt
+++ debug.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 (require (for-syntax racket/base racket/syntax))
 
-(provide report)
+(provide report report*)
 
 (define-syntax (report stx)
   (syntax-case stx ()
@@ -10,3 +10,7 @@
      #'(begin 
          (displayln (format "~a = ~v" 'name expr) (current-error-port)) 
          expr)]))
+
+(define-syntax (report* stx)
+  (syntax-case stx ()
+    [(_ expr ...) (datum->syntax stx `(begin ,@(map (λ(arg) `(report ,arg)) (syntax->datum #'(expr ...)))))]))
