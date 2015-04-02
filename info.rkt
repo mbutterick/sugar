d1a738c4f2e9af579287e3a4c794692eba78d107
Matthew Butterick <mb@mbtype.com>
2015-04-01 20:45:39 -0700

    move tests

--- info.rkt
+++ info.rkt
@@ -3,4 +3,4 @@
 (define deps '(("base" #:version "6.0")))
 (define build-deps '("scribble-lib"))
 (define scribblings '(("scribblings/sugar.scrbl" ())))
-(define compile-omit-paths '("tests.rkt"))
+(define compile-omit-paths '("test"))
