b769691bdef9c6ecc7e0bd482743c643e1d6bc97
Matthew Butterick <mb@mbtype.com>
2015-04-21 22:36:54 -0700

    add 'safe submodule for sugar/xml

--- sugar/xml.rkt
+++ sugar/xml.rkt
@@ -1,12 +1,12 @@
 #lang racket/base
-(require xml racket/port racket/contract)
+(require xml racket/port racket/contract "define.rkt")
 (provide (all-defined-out))
 
-(define/contract (xml-string->xexprs str)
+(define+provide+safe (xml-string->xexprs str)
   (string? . -> . (values xexpr? xexpr?))
   (define xml-doc (with-input-from-string str (λ _ (permissive-xexprs #t) (read-xml))))
   (values (xml->xexpr (document-prolog xml-doc)) (xml->xexpr (document-element xml-doc))))
 
-(define/contract (xexprs->xml-string prolog-xexpr root-xexpr)
+(define+provide+safe (xexprs->xml-string prolog-xexpr root-xexpr)
   (xexpr? xexpr? . -> . string?)
   (with-output-to-string (λ _ (write-xml (document (xexpr->xml prolog-xexpr) (xexpr->xml root-xexpr) null)))))
