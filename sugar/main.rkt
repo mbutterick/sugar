293defb2d9dd572848b882480cbbdcf2d3aec5d7
Matthew Butterick <mb@mbtype.com>
2015-05-23 07:36:47 -0700

    create safe submod at top level

--- sugar/main.rkt
+++ sugar/main.rkt
@@ -1,30 +1,22 @@
 #lang racket/base
 
-(require
-  "cache.rkt"
-  "coerce.rkt"
-  "container.rkt"
-  "debug.rkt"
-  "define.rkt"
-  "file.rkt"
-  "include.rkt"
-  "len.rkt"
-  "list.rkt"
-  "misc.rkt"
-  "string.rkt"
-  "xml.rkt")
+(define-syntax-rule (r+p modname)
+  (begin
+    (require modname)
+    (provide (all-from-out modname))
+    (module+ safe
+      (require (submod modname safe))
+      (provide (all-from-out (submod modname safe))))))
 
-(provide 
- (all-from-out
-  "cache.rkt"
-  "coerce.rkt"
-  "container.rkt"
-  "debug.rkt"
-  "define.rkt"
-  "file.rkt"
-  "include.rkt"
-  "len.rkt"
-  "list.rkt"
-  "misc.rkt"
-  "string.rkt"
-  "xml.rkt"))
+(r+p "cache.rkt")
+(r+p "coerce.rkt")
+(r+p "container.rkt")
+(r+p "debug.rkt")
+(r+p "define.rkt")
+(r+p "file.rkt")
+(r+p "include.rkt")
+(r+p "len.rkt")
+(r+p "list.rkt")
+(r+p "misc.rkt")
+(r+p "string.rkt")
+(r+p "xml.rkt")
