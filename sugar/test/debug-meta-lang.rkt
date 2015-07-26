65467b72b4e66299b880ca60fde35dfa5ec98d4d
AlexKnauth <alexander@knauth.org>
2015-07-26 00:33:34 -0400

    debug-meta-lang: make (require sugar/debug) unnecessary
    and make it hygienic

--- sugar/test/debug-meta-lang.rkt
+++ sugar/test/debug-meta-lang.rkt
@@ -1,10 +1,15 @@
 #lang sugar/debug/lang racket
-(require sugar/debug rackunit)
-(let ([out (open-output-string)])
+(require rackunit)
+(let ([out (open-output-string)]
+      [let "something else"]
+      [local-require "something else entirely"]
+      [only-in "completely unexpected!"]
+      [report "well, not really"])
   (parameterize ([current-error-port out])
     #^5)
   (check-equal? (get-output-string out) "5 = 5\n"))
-(let ([out (open-output-string)])
+(let ([out (open-output-string)]
+      [report/line "outta the blue!"])
   (parameterize ([current-error-port out])
     #^^5)
-  (check-equal? (get-output-string out) "5 = 5 on line 9\n"))
+  (check-equal? (get-output-string out) "5 = 5 on line 14\n"))
