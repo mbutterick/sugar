6a68eb663db27c4cf0c267d2a8c70b474f557eab
Alex Knauth <alexander@knauth.org>
2015-10-10 12:30:39 -0400

    Add test for sugar/debug shorthand in phase 3

--- sugar/test/debug-meta-lang.rkt
+++ sugar/test/debug-meta-lang.rkt
@@ -1,5 +1,10 @@
 #lang sugar/debug racket
-(require rackunit)
+(require rackunit
+         (for-meta 1 (only-in racket/base begin-for-syntax))
+         (for-meta 2 (only-in racket/base begin-for-syntax))
+         (for-meta 3 (only-in racket/base let #%app open-output-string get-output-string parameterize
+                              current-error-port #%datum)
+                   rackunit))
 (let ([out (open-output-string)]
       [let "something else"]
       [local-require "something else entirely"]
@@ -12,4 +17,12 @@
       [report/line "outta the blue!"])
   (parameterize ([current-error-port out])
     #RR5)
-  (check-equal? (get-output-string out) "5 = 5 on line 14\n"))
+  (check-equal? (get-output-string out) "5 = 5 on line 19\n"))
+
+(begin-for-syntax
+  (begin-for-syntax
+    (begin-for-syntax
+      (let ([out (open-output-string)])
+        (parameterize ([current-error-port out])
+          #RR5)
+        (check-equal? (get-output-string out) "5 = 5 on line 27\n")))))
