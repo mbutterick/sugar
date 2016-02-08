aa0d885a6f468aba62ce10ce41d825baea0fc975
Matthew Butterick <mb@mbtype.com>
2016-02-08 15:33:42 -0800

    more

--- unstable/include.rkt
+++ unstable/include.rkt
@@ -6,24 +6,6 @@
                      racket/match racket/function)
          "../define.rkt")
 
-(module+ test
-  (require rackunit)
-  (module include-test racket/base
-   (require sugar/include)
-   (include-without-lang-line "source.rkt")
-   (provide included-symbol))
-
- (require 'include-test)
- (check-equal? included-symbol 'bar)
-
- (module no-lang-line-include-test racket/base
-   (require sugar/include)
-   (include-without-lang-line "no-lang-line-source.txt")
-   (provide no-lang-symbol))
-
- (require 'no-lang-line-include-test)
- (check-equal? no-lang-symbol 'bar))
-
 (provide+safe include-without-lang-line)
 
 (define-syntax (do-include stx)
