6f4bbeefe60e2c451d16a736b49ab020ce98e603
Matthew Butterick <mb@mbtype.com>
2016-03-27 17:23:33 -0700

    adjust info.rkt

--- info.rkt
+++ info.rkt
@@ -3,5 +3,3 @@
 (define version "0.2")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("sugar/scribblings/sugar.scrbl" ())))
-(define compile-omit-paths '("sugar/test"))
---
+++ sugar/info.rkt
@@ -0,0 +1,3 @@
+#lang info
+(define scribblings '(("scribblings/sugar.scrbl" ())))
+(define compile-omit-paths '("test"))
